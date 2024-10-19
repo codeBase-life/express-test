6a3e811da5cd5eba50662dc38b8d5f6844ad1e58
malik rasheed <codedev366@gmail.com>
2024-10-20 01:02:10 +0500

    some new routes added

--- api/index.js
+++ api/index.js
@@ -5,7 +5,13 @@ const port = 3000;
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+app.get("/user", (req, res) => {
+  res.send("<h1>hello new user 🙂</h1>");
+});
 
+app.get("/login", (req, res) => {
+  res.send("<h3>you have now logged in 😉</h3>");
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
